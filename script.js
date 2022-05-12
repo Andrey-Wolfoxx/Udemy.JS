2768009513df4a2425fe1aa42baa6fdfe13cb004
Andrey <jandtai75@gmail.com>
2022-05-12 19:16:40 +0300

    12.05.2022

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Hello world!');
+'use strict';
+
+const numberOfFilms = +prompt("How many films you've watched?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt("Last film?", ""),
+	  b = prompt("Rate it", ""),
+	  c = prompt("Last film?", ""),
+	  d = prompt("Rate it", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+let obj = {
+	1:1, 
+	0:0
+};
+
+for(let key in obj) {
+	console.log(key);
+}
+
+function firstLife () {  
+	alert('Hello world!');
+}
